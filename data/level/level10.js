f18c38406cf5e64724e125f54da7b88c1db7bc56
superpippoub77 <superpippoub77@gmail.com>
2026-02-01 21:36:57 +0100

    check and fix

--- data/level/level10.js
+++ data/level/level10.js
@@ -4,13 +4,13 @@ window.LEVEL_MAP = {
   cols: 12,
   rows: 8,
   map: [
-    ['sand1','sand1','sand2','sand2','sand3','sand3','sand4','sand4','sand5','sand5','sand6','sand6'],
-    ['sand1','sand1','sand2','sand2','sand3','hole' ,'sand4','sand4','sand5','sand5','sand6','sand6'],
-    ['sand7','sand1','sand2','sand3','sand4','sand5','sand6','sand7','sand8','sand9','sand10','sand1'],
-    ['sand2','sand2','sand3','sand3','sand4','sand4','sand5','sand5','sand6','sand6','sand7','sand7'],
-    ['sand8','sand8','sand9','sand9','sand10','sandPile','sand1','sand2','sand3','sand4','sand5','sand6'],
-    ['sand1','sand2','sand3','hole' ,'sand5','sand6','sand7','sand8','sand9','sand10','sand1','sand2'],
-    ['sand3','sand4','sand5','sand6','sand7','sand8','sand9','sand10','sand1','sand2','sand3','sand4'],
-    ['sand5','sand6','sand7','sand8','sand9','sand10','sand1','sand2','sand3','sand4','sand5','sand6']
+    ['sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10'],
+    ['sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10'],
+    ['sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10'],
+    ['sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10'],
+    ['sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10'],
+    ['sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10'],
+    ['sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10'],
+    ['sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10','sand10']
   ]
 };
